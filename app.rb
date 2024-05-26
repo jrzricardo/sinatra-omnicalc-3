19eab28fdaf637a86ab10212a43c673e7bc3f92d
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-05-26 11:58:20 +0000

    concealed api key

--- app.rb
+++ app.rb
@@ -16,9 +16,10 @@ get("/umbrella") do
 end
 
 get("/process_umbrella") do
+  gmaps_key = ENV.fetch("GMAPS_KEY")
   @user_location = params.fetch("user_loc")
 
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=" + gmaps_key
 
   @raw_response = HTTP.get(gmaps_url).to_s
  
