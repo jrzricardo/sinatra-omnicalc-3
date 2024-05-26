7114f3e6a4ad53b79633333795d3802807062154
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-05-26 17:11:27 +0000

    building umbrella next

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 
+gmaps_key = ENV.fetch("GMAPS_KEY")
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
@@ -16,10 +18,12 @@ get("/umbrella") do
 end
 
 get("/process_umbrella") do
-  gmaps_key = ENV.fetch("GMAPS_KEY")
+ 
   @user_location = params.fetch("user_loc")
 
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=" + gmaps_key
+  url_encoded_string = @user_location.gsub(" ", "+")
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=" + gmaps_key
 
   @raw_response = HTTP.get(gmaps_url).to_s
  
@@ -30,5 +34,35 @@ get("/process_umbrella") do
   @latitude = @location_hash.fetch("lat")
   @longitude = @location_hash.fetch("lng")
 
-  erb(:umbrella_results)
+  pirate_url = "https://api.pirateweather.net/forecast/#{pirate_weather_key}/#{@latitude},#{@longitude}"
+ 
+  @raw_pirate_response = HTTP.get(pirate_url).to_s
+
+  @parsed_pirate_response = JSON.parse(@raw_pirate_response)
+  
+  @currently_hash = @parsed_pirate_response.fetch("currently")
+
+  @temperature = @currently_hash.fetch("temperature")
+
+  @summary = @currently_hash.fetch("summary")
+
+  @hour_hash = @parsed_pirate_response.fetch("hourly")
+
+  @hour_data_array = @hour_hash.fetch("data")
+  
+  @next_twelve_hours = @hour_data_array[1..12]
+  
+  @precip_prob_threshold = 0.10
+
+  @any_precipitation = false
+
+
+  @next_tweleve_hours.each do | an_hour |
+    @precip_prob = an_hour.fetch("precipProbability")
+
+  if @precip_prob > @precip_prob_threshold
+    @any_precipitation = true
+  end
+end
+
 end
